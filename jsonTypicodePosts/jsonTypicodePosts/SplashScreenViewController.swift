b10417657f30e7ed0c1eb67c7dbc13822306fd69
Christian Esomnofu <mresomnofu@gmail.com>
2020-05-31 05:18:44 +0100

    initialization: start of project structuring

---
+++ jsonTypicodePosts/jsonTypicodePosts/SplashScreenViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jsonTypicodePosts
+//
+//  Created by Malik on 31/05/2020.
+//  Copyright © 2020 MrOfu. All rights reserved.
+//
+
+import UIKit
+
+class SplashScreenViewController: UIViewController {
+
+    //MARK: Properties
+    
+    
+    //MARK: Init
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: Handlers
+
+
+}
+
